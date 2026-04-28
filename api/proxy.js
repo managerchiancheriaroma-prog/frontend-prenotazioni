44cfd3075af299e10d2e936d1d50caa51a7c1cb2
managerchiancheriaroma-prog <managerchiancheriaroma@gmail.com>
2026-04-28 19:59:48 +0200

    Update GAS_URL in proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
 
-  const GAS_URL = "https://script.google.com/macros/s/AKfycbyAexEOGdH3phKjLyXkXAVY-aT8SdoCsUMVaIyhoj4Eo9kpxsVEoyoif1A_N4nvzOiT/exec";
+  const GAS_URL = "https://script.google.com/macros/s/AKfycbwVSzRV97rRnRHWodotJEgjW6kDIFNvPajnpZXcR5Thljogm6C65x4sU4IfHjA9ZoHt/exec";
 
   try {
 
