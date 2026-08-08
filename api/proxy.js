5f6747d535c7d17a8cc6a2996a1e32f23cdb5798
managerchiancheriaroma-prog <managerchiancheriaroma@gmail.com>
2026-08-08 22:14:56 +0200

    Update GAS_URL in proxy handler function dopo fix colonna file google

--- api/proxy.js
+++ api/proxy.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
 
-  const GAS_URL = "https://script.google.com/macros/s/AKfycbyLxpxzEYie56VpOtXHeS67G_2_aA4KlWjRtJqf0MpfdX-8NlXZvv2lljrZZNg_ZLjU/exec";
+  const GAS_URL = "https://script.google.com/macros/s/AKfycbwOwFdAYlQn61BOHN3wBVxInrVHd0vNgOvK04hKrMCZlubMmXsec-ZqNIn4YjRzyE-K/exec";
 
   console.log("GAS CHIAMATO:", GAS_URL);
 
